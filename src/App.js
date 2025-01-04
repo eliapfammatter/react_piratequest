2d7584d4786d60c89f1ab68e147d393be2eb6b54
Elia Pfammatter <elia.pfammatter@students.hevs.ch>
2025-01-04 16:37:16 +0100

    flow diagram

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
         <Route path="/" element={<DynamicPage slug="description" />} />
         <Route path="/sketch" element={<DynamicPage slug="sketch" />} />
         <Route path="/mockup" element={<DynamicPage slug="mockup" />} />
-        <Route path="/flowdiagram" element={<DynamicPage slug="flowdiagram" />} />
+        <Route path="/flowdiagram" element={<DynamicPage slug="flow-diagram" />} />
         <Route path="/logbook" element={<DynamicPage slug="logbook" />} />
         <Route path="/game" element={<DynamicPage slug="game" />} />
         <Route path="/forms" element={<DynamicPage slug="forms" />} />
